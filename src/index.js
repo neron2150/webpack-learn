3dd667a0cba5a061deed379c2869dc63517bf14b
neron2150 <neron2150@gmail.com>
2019-03-25 12:59:30 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ import externalFunc from './another.js';
 
 externalFunc('hello, i\'m external');
 const element = document.getElementById('element');
-console.log(element);
 element.innerHTML = 'Hello, world!';
-element.style = '100px';
